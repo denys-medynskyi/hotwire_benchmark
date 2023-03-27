ab73d0acdb2e49b8f39ba6ca56924a9bdc309a33
Denys Medynskyi <denys.medynskyi@gmail.com>
2023-03-27 16:43:01 +0300

    Add todo check

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "todos#index"
+
+  resources :todos do
+    member do
+      post :toggle
+    end
+  end
 end
